edd52298e205a1f0396dfe0b8d5b682aaa380978
Jaren-Wheeler <83668434+Jaren-Wheeler@users.noreply.github.com>
2026-03-26 15:31:28 -0600

    Implement count_classes_per_course function

--- functions/public/count_classes_per_course.sql
+++ functions/public/count_classes_per_course.sql
@@ -1,1 +1,16 @@
 --TODO Implement function
+
+CREATE OR REPLACE FUNCTION count_classes_per_course (
+    p_course_id IN Classes.course_id%TYPE
+)
+RETURN number
+IS
+   v_row_count number;
+BEGIN
+    SELECT count(*)
+    INTO v_row_count
+    FROM Classes
+    WHERE course_id = p_course_id;
+    RETURN v_row_count;
+END count_classes_per_course;
+/
