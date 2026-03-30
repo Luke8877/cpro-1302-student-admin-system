ca64952570b73c7cf99aa920f5c5f63e99a70a20
Jaren-Wheeler <83668434+Jaren-Wheeler@users.noreply.github.com>
2026-03-29 18:42:29 -0600

    Create convert_grade function

--- functions/public/convert_grade.sql
+++ functions/public/convert_grade.sql
@@ -1,1 +1,23 @@
 --TODO Implement function
+
+CREATE OR REPLACE FUNCTION convert_grade (
+    p_grade IN NUMBER
+)
+RETURN VARCHAR2
+IS
+    v_letter_grade VARCHAR2(1);
+BEGIN
+    IF p_grade >= 90 THEN
+            RETURN 'A';
+        ELSIF p_grade >= 80 THEN
+            RETURN 'B';
+        ELSIF p_grade >= 70 THEN
+            RETURN 'C';
+        ELSIF p_grade >= 60 THEN
+            RETURN 'D';
+        ELSE
+            RETURN 'F';
+    END IF;
+   
+END;
+/
